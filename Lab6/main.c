a366c3522f62767b194b08f6701bb19dfd9410e8
RTLennox <rtl11@zips.uakron.edu>
2016-11-04 12:08:18 -0400

    Initial Commit

---
+++ Lab6/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab6
+//
+//  Created by Tyler Lennox on 11/4/16.
+//  Copyright © 2016 Tyler Lennox. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
